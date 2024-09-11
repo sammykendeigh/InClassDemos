6c13cb28d0c8b40319fb1c62ac7b709eea784eeb
stephanievalentine <stephanievalentine@users.noreply.github.com>
2024-09-11 10:29:58 -0500

    Create PrintFormatting.java
    before day 7

---
+++ src/edu/unl/raikes/day7/PrintFormatting.java
@@ -0,0 +1,24 @@
+/** 
+ * This is PrintFormatting.java, packaged within 
+ * edu.unl.raikes.day7. 
+ */
+package edu.unl.raikes.day7;
+
+/** 
+ * This class provides the definitions for 
+ * PrintFormatting in the edu.unl.raikes.day7 package.
+ * 
+ */
+public class PrintFormatting {
+
+    /** 
+     * This function ... TODO
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
