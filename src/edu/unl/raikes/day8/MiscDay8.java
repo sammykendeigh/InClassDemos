bc1a8a6dbff78767af569606bf7313303e3dfacd
stephanievalentine <stephanievalentine@users.noreply.github.com>
2024-09-18 10:28:42 -0500

    day 8, start of day 9

---
+++ src/edu/unl/raikes/day8/MiscDay8.java
@@ -0,0 +1,26 @@
+package edu.unl.raikes.day8;
+
+/**
+ * This class contains a game that allows users to play MadLibs.
+ */
+public class MiscDay8 {
+
+    /**
+     * This will hold the cool questions we answer in class.
+     *
+     * @param args This program does not use any arguments.
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int x = 6;
+
+        if (x < 6) {
+            x = x + 1;
+        } else if (x > 6) {
+            System.out.println("greater than 6");
+        }
+
+        System.out.println(x);
+    }
+
+}
---
+++ src/edu/unl/raikes/day9/MiscDay9.java
@@ -0,0 +1,17 @@
+package edu.unl.raikes.day9;
+
+/**
+ * This class contains a game that allows users to play MadLibs.
+ */
+public class MiscDay9 {
+
+    /**
+     * This will hold the cool questions we answer in class.
+     *
+     * @param args This program does not use any arguments.
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+}
