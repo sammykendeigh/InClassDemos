ebe1b607b5f23242a820a41dd83cd0960306be98
stephanievalentine <stephanievalentine@users.noreply.github.com>
2024-09-25 09:24:59 -0500

    prep for day 12

---
+++ src/unl/edu/raikes/day12/Aaaaaaaaaa.java
@@ -0,0 +1,32 @@
+package unl.edu.raikes.day12;
+
+import java.util.Scanner;
+
+/**
+ * This is Aaaaaaaaaa.java, packaged within unl.edu.raikes.day12.
+ */
+public class Aaaaaaaaaa {
+
+    /**
+     * The main method.
+     *
+     * @param args This program does not accept any arguments.
+     */
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        // ask the user for the size
+        System.out.print("please enter an int:");
+        int userNum = scnr.nextInt();
+
+        for (int row = 1; row <= userNum; row++) {
+            for (int col = 1; col <= userNum; col++) {
+                System.out.print(".");
+            }
+            System.out.println();
+        }
+
+        scnr.close();
+    }
+
+}
