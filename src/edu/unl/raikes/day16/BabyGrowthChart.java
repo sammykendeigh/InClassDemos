e8fb3b032ffc896bb0cf984f91d3e5cce67709ad
stephanievalentine <stephanievalentine@users.noreply.github.com>
2024-10-04 12:00:05 -0500

    after class day 16

--- src/edu/unl/raikes/day16/BabyGrowthChart.java
+++ src/edu/unl/raikes/day16/BabyGrowthChart.java
@@ -15,11 +15,10 @@ public class BabyGrowthChart {
      */
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
+
         // accumulator variables
         double allTimeGrowth = 0;
         double largestSpurt = 0;
-
-        int[][] a = { { 2, 3, 4 }, { 2, 3 } };
 
         // get bounds for our 2d array
         System.out.print("How many days of measurements do you have to analyze? ");
@@ -27,14 +26,41 @@ public class BabyGrowthChart {
         final int NUM_LENGTHS_PER_DAY = 3;
 
         // what should our array initialization look like?
+        int[][] lengths = new int[DAYS_OF_MEASUREMENTS][NUM_LENGTHS_PER_DAY];
+        int[] averageLengths = new int[DAYS_OF_MEASUREMENTS];
 
-        // how do we average the lengths? Share it with the user!
+        // how do we get the lengths from the user?
+        for (int row = 0; row < lengths.length; row++) {
+            averageLengths[row] = 0;
+
+            System.out.println("Now we're talking about day " + (row + 1));
+            for (int col = 0; col < lengths[row].length; col++) {
+                System.out.print("    Measurement " + (col + 1) + ": ");
+                lengths[row][col] = scnr.nextInt();
+                averageLengths[row] += lengths[row][col];
+            }
+
+            // divide average by the number measurements taken
+            averageLengths[row] = averageLengths[row] / lengths[row].length;
+            System.out.println("    Average Measurement: " + averageLengths[row]);
+
+            // see if this day is growth from prev day
+            if (row > 0 && averageLengths[row] > averageLengths[row - 1]) {
+                allTimeGrowth += averageLengths[row] - averageLengths[row - 1];
+            }
+        }
 
         // how can we calculate the growth from the previous day? Share it with the user!
 
         // how can we calculate the growth over all time? Share it with the user!
 
         // how can we print the values so we can see them?
+        for (int row = 0; row < lengths.length; row++) {
+            for (int col = 0; col < lengths[row].length; col++) {
+                System.out.print(lengths[row][col] + "   ");
+            }
+            System.out.println();
+        }
 
         System.out.printf("Since the first measurements, this baby has grown %.2f inches.\n", allTimeGrowth);
         System.out.printf("This baby's largest growth spurt was %.2f inches.", largestSpurt);
