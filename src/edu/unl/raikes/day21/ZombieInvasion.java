98552c1f7395bfd6060d37a391320f8a0e85696b
stephanievalentine <stephanievalentine@users.noreply.github.com>
2024-10-16 10:30:58 -0500

    zombies!!

---
+++ src/edu/unl/raikes/day21/ZombieInvasion.java
@@ -0,0 +1,24 @@
+package edu.unl.raikes.day21;
+
+/**
+ * This is ZombieInvasion.java, packaged within edu.unl.raikes.day21.
+ */
+public class ZombieInvasion {
+
+    /**
+     * The main method.
+     *
+     * @param args This program does not accept any arguments.
+     */
+    public static void main(String[] args) {
+        String townName = "town name";
+        String mayorName = "mayor name";
+        double x = 0;
+        double y = 0;
+
+        System.out.println("Aaaaahhh!!! Zombie invasion! ");
+        System.out.println("This is " + townName + "central command. We need to warn our neighbors.");
+
+    }
+
+}
