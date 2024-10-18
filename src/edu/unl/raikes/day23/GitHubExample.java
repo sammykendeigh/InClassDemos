303da7e12ba941e2330e8ac2c9b41260f1586685
stephanievalentine <stephanievalentine@users.noreply.github.com>
2024-10-18 10:10:09 -0500

    Create GitHubExample.java

---
+++ src/edu/unl/raikes/day23/GitHubExample.java
@@ -0,0 +1,17 @@
+package edu.unl.raikes.day22;
+
+/**
+ * This is GitHubExample.java, packaged within edu.unl.raikes.day22.
+ */
+public class GitHubExample {
+
+    /**
+     * The main method.
+     *
+     * @param args This program does not accept any arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
